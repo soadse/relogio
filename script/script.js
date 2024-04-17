674d83d078746d9c84ea77d9d0b5e0e0848950fe
Rodrigo Andrade <144704436+soadse@users.noreply.github.com>
2024-04-17 18:24:15 -0300

    .

--- script/script.js
+++ script/script.js
@@ -1,21 +1,41 @@
-var hora = document.querySelector('#hora')
-var minuto = document.querySelector('#minutos')
-var segundo = document.querySelector('#segundos')
-var dia = document.querySelector('#dia')
-var mes = document.querySelector('#mes')
-var ano = document.querySelector('#ano')
-var semana = document.querySelector('#semana')
-var agora = new Date();
+var corpo = window.document.body
+var saudacao = window.document.querySelector('#saudacao')
+const relogio = setInterval(function time() {
+    var hora = document.querySelector('#hora')
+    var minuto = document.querySelector('#minutos')
+    var segundo = document.querySelector('#segundos')
+    var agora = new Date();
+    var hr = agora.getHours();
+    var min = agora.getMinutes();
+    var seg = agora.getSeconds();
 
-var horas = agora.getHours();
-var minutos = agora.getMinutes();
-var segundos = agora.getSeconds();
-hora.innerText = `${horas}`
-minuto.innerText = `${minutos}`
-segundo.innerText = `${segundos}`
+    if(hr < 10) hr = '0' + hr;
+    if(min < 10) min = '0' + min;
+    if(seg < 10) seg = '0' + seg;
+
+    hora.innerText = `${hr}`
+    minuto.innerText = `${min}`
+    segundo.innerText = `${seg}`
+
+    if(hr < 12){
+        corpo.style.background = 'lightblue'
+        saudacao.innerText = 'Bom Dia'
+    } else if(hr >= 12 && min >= 0 && hr < 18){
+        corpo.style.background = 'orange'
+        saudacao.innerText = 'Boa Tarde'
+    } else if(hr >= 18) {
+        corpo.style.background = 'darkblue'
+        saudacao.innerText = 'Boa Noite'
+    }
+})
+
+
+
 
 // ATUALIZA O DIA DA SEMANA
 function weeks(){
+    var agora = new Date();
+    var semana = document.querySelector('#semana')
     var semanas = agora.getDay();
     switch (semanas){
         case 0:
@@ -44,44 +64,19 @@ function weeks(){
 setInterval(weeks, 1000)
 
 
-// ATUALIZA AS HORAS
-function contarTempo(){
-    segundo.innerText = `${segundos}`
-    minuto.innerText = `${minutos}`
-    hora.innerText = `${horas}`
-    segundos++
-    if(segundos == 60){
-        segundos = -1
-        minutos += 1
-    } else if(segundos < 10){
-        segundo.innerText = `0${segundos}`
-    }
-
-    if(minutos == 60){
-        minutos = 0
-        horas += 1
-    } else if(minutos < 10){
-        minuto.innerText = `0${minutos}`
-    }
-
-    if(horas == 24){
-        horas = 0
-    }
-}
-setInterval(contarTempo, 1000)
-
-
 // ATUALIZA O CALENDÁRIO
 function contarDia(){
+    var dia = document.querySelector('#dia')
+    var mes = document.querySelector('#mes')
+    var ano = document.querySelector('#ano')
+    var agora = new Date();
     var dias = agora.getDate();
-    var meses = agora.getMonth();
+    var meses = agora.getMonth()+1;
     var anos = agora.getFullYear(); 
-    if(dias < 10){
-        dias = `0${dias}`
-    }
-    if(meses < 10){
-        meses = `0${meses+1}`
-    }
+    
+    if(dias < 10) dias = '0' + dias;
+    if(meses < 10) meses = '0' + meses;
+
     dia.innerText = `${dias}`
     mes.innerText = `${meses}`
     ano.innerText = `${anos}` 
